3b39104178ca3e25bf09fa91ac5e678f67c6a23c
James Carson <jms.crsn@gmail.com>
2015-09-20 20:06:08 +0000

    fixed some &[u8] conversion to string stupidity. reading the docs helps!

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,15 @@ fn client_connection(mut stream: TcpStream) {
             Ok(0) => break,
             Ok(bytes_read) => {
                 let ( data, _ ) = read_slice.split_at(bytes_read);
-                let s = String::from_utf8(data.to_vec()).unwrap();
-                buffer.push_str(&s);
+                let text = std::str::from_utf8(data).unwrap();
+                buffer.push_str(text);
                 let temp = buffer.clone();
                 let mut messages:Vec<&str> = temp.split("\r\n\r\n").collect();
                 if messages.len() > 1 {
                     let remainder = messages.pop().unwrap();
                     for message in messages {
-                        println!("got message\n{}",message);
-                        let s = message.to_string();
-                        let lines:Vec<&str> = s.lines().collect();
+                        println!("----------------------------------------------\n{}",message);
+                        let lines:Vec<&str> = message.lines().collect();
                         if lines[0] == "GET / HTTP/1.1" {
                             let _ = stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 38\r\n\r\n<html><body><h1>OK</h1></body></html>\n");
                         } else {
@@ -37,7 +36,7 @@ fn client_connection(mut stream: TcpStream) {
 }
 
 fn main() {
-    let listener = TcpListener::bind("104.236.40.97:80").unwrap();
+    let listener = TcpListener::bind("104.236.40.97:3000").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
