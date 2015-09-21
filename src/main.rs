574407174d8eb6d1a81fa0ef48843577f039f333
James Carson <jms.crsn@gmail.com>
2015-09-21 04:53:13 +0000

    added some simple logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::prelude::*;
+use std::fs::{OpenOptions};
+
+enum LogLevel {
+    Info,
+    Error,
+}
+
+fn write_to_log_file(level: LogLevel, message: &str) {
+    let mut log_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(true)
+        .open("/var/log/simple_rust_webserver.log").unwrap();
+    let prefix = match level {
+        LogLevel::Info => "[INFO]",
+        LogLevel::Error => "[ERROR]",
+    };
+    let _ = writeln!(log_file,"{}: {}",prefix,message);
+}
 
 fn client_connection(mut stream: TcpStream) {
     let mut buffer = String::new();
@@ -9,7 +28,10 @@ fn client_connection(mut stream: TcpStream) {
 
     loop {
         match stream.read(read_slice) {
-            Ok(0) => break,
+            Ok(0) => {
+                write_to_log_file(LogLevel::Error,"Read 0 Bytes. Ending Read Loop");
+                break;
+            },
             Ok(bytes_read) => {
                 let ( data, _ ) = read_slice.split_at(bytes_read);
                 let text = std::str::from_utf8(data).unwrap();
@@ -19,7 +41,7 @@ fn client_connection(mut stream: TcpStream) {
                 if messages.len() > 1 {
                     let remainder = messages.pop().unwrap();
                     for message in messages {
-                        println!("----------------------------------------------\n{}",message);
+                        write_to_log_file(LogLevel::Info,message);
                         let lines:Vec<&str> = message.lines().collect();
                         if lines[0] == "GET / HTTP/1.1" {
                             let _ = stream.write(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 38\r\n\r\n<html><body><h1>OK</h1></body></html>\n");
@@ -30,7 +52,10 @@ fn client_connection(mut stream: TcpStream) {
                     buffer = remainder.to_string();
                 }
             },
-            Err(_) => break,
+            Err(_) => {
+                write_to_log_file(LogLevel::Error,"Error Reading Socket. Ending Read Loop");
+                break
+            },
         }
     }
 }
